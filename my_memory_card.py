f3e6da1eda9c3465714107c6a9e3a3f315dbc353
TapeAlligator <105974527+TapeAlligator@users.noreply.github.com>
2022-05-21 14:45:21 +0300

    Add files via upload

---
+++ my_memory_card.py
@@ -0,0 +1,27 @@
+#создай приложение для запоминания информации
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QGroupBox, QRadioButton 
+
+app = QApplication([])
+main_win = QWidget()
+main_win.setWindowTitle('Опрос')
+a = QLabel('a')
+ads = QGroupBox("Варианты ответов")
+rbtn_1 = QRadioButton('Энцы')
+rbtn_2 = QRadioButton('Смурфы')
+rbtn_3 = QRadioButton('Чулымцы')
+rbtn_4 = QRadioButton('Алеуты')
+layout_ans1 = QHBoxLayout()
+layout_ans2 = QVBoxLayout()
+layout_ans3 = QVBoxLayout()
+
+layout_ans2.addWidget(rbtn_1)
+layout_ans2.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_3)
+layout_ans3.addWidget(rbtn_4)
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3)
+
+main_win.setLayout(layout_ans1)
+main_win.show()
+app.exec_()
